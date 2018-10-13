77cdac72ece4fac6bfffb3eeb4301c6821f75beb
WallKroftPanda <rorro2997@gmail.com>
2018-10-13 18:09:29 -0300

    Folders and main

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <assimp/cimport.h> // C importer
+#include <assimp/scene.h> // collects data
+#include <assimp/postprocess.h> // various extra operations
+#include <GL/glew.h> // include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <string>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+//VARIABLES GLOBALES
+
+int g_gl_width = 1280;
+int g_gl_height = 720;
+GLFWwindow* g_window = NULL;
+
+void processInput(GLFWwindow *window);
+
+int main()
+{
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* g_window = glfwCreateWindow(g_gl_width, g_gl_height, "Bodoque's Air Raid", NULL, NULL);
+
+    while (!glfwWindowShouldClose(g_window)){
+        glfwSwapBuffers(g_window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
